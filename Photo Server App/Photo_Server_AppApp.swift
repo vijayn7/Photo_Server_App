8cccddfbe211ae79de0c495c74eefd137c78903d
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2025-08-14 18:50:42 -0400

    First Commit

---
+++ Photo Server App/Photo_Server_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Server_AppApp.swift
+//  Photo Server App
+//
+//  Created by Vijay Raju Nannapuraju on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Server_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photo Server AppTests/Photo_Server_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Server_AppTests.swift
+//  Photo Server AppTests
+//
+//  Created by Vijay Raju Nannapuraju on 8/14/25.
+//
+
+import Testing
+@testable import Photo_Server_App
+
+struct Photo_Server_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
